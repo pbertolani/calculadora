84a7fce9b8242c11248e4f9e62fd1a9a0783abc4
pbertolani <pedrobertolani22@gmail.com>
2026-01-04 15:56:11 -0300

    second commit

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function rank(victories, defeat){
     total = victories-defeat;
     return total;
 }
+
+if (total < 10){
+    level = "Ferro";
+    console.log(`O Herói tem de saldo de ${saldo} e está no nível de ${nivel}`);
+}
+
+else if (10 < total && total < 21){
+    level = "Bronze";
+    console.log(`O Herói tem de saldo de ${saldo} e está no nível de ${nivel}`);
+}
