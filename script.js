7d3c9b60a9769203a45b78083533d7e98e0f9d11
pbertolani <pedrobertolani22@gmail.com>
2026-01-04 15:41:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Calculadora de Nivel
+// Dev: Pedro Bertolani
+// Data: 04/01/2026
+
+rank (15,1);
+var level = "";
+
+function rank(victories, defeat){
+    total = victories-defeat;
+    return total;
+}
